a6aefe1c569b61246b4aa50db6b4b12a99fde381
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2022-08-22 10:25:06 -0700

    Update macOS minimum version to 10.11

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Differ",
     platforms: [
         .iOS(.v9),
-        .macOS(.v10_12),
+        .macOS(.v10_11),
         .tvOS(.v9),
         .watchOS(.v4)
     ],
